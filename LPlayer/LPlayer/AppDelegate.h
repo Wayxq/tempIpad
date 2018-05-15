b1bc246c5d61a79b774acc587f8fdea501600d78
way <shanguo00@shanguo00s-iMac.local>
2018-05-15 13:54:12 +0800

    first commit

---
+++ LPlayer/LPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LPlayer
+//
+//  Created by way on 2018/5/15.
+//  Copyright © 2018年 way. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LPlayer/LPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LPlayer
+//
+//  Created by way on 2018/5/15.
+//  Copyright © 2018年 way. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
